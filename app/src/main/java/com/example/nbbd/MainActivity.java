a40e00420831ebce052828aeda81cbc16ab67f23
Nothing Nothing <nothing@nothing>
2025-03-10 13:30:00 +0200

    makinf the button work as asked

--- app/src/main/java/com/example/nbbd/MainActivity.java
+++ app/src/main/java/com/example/nbbd/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.nbbd;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
+        ImageButton imb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imb = findViewById(R.id.imb);
+    }
+
+    public void onPush(View view) {
+        int[] id = new int[3];
+        id[0] = R.drawable.one;
+        id[1] = R.drawable.two;
+        id[2] = R.drawable.three;
+
+        int x = (int)(Math.random() * 3);
+        imb.setImageResource(id[x]);
+
     }
 }
